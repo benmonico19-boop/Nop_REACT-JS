4b79433b691495b2cc0fb47b7766a22b5ce8519c
Benmonico <benmonico@gmail.com>
2026-06-10 15:33:12 +0700

    Disable sourcemap and fix build config

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: '/Nop_REACT-JS/',
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    minify: 'esbuild',
+  },
 })
